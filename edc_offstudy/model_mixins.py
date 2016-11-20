8b0f6c61806dda4bdc782ee738e1beeaa659fee0
erikvw <ew2789@gmail.com>
2016-11-21 01:35:16 +0200

    verify offstudy date against visit date, fix delete appts on offstudy

--- edc_offstudy/model_mixins.py
+++ edc_offstudy/model_mixins.py
@@ -1,15 +1,14 @@
 from django.apps import apps as django_apps
 from django.db import models
+from django.db.models import Max
 from django.utils import timezone
 
 from edc_base.model.fields import OtherCharField
-from edc_base.model.validators import date_not_future
-from edc_protocol.validators import date_not_before_study_start
+from edc_base.model.validators import datetime_not_future
+from edc_protocol.validators import datetime_not_before_study_start
 from edc_registration.model_mixins import SubjectIdentifierModelMixin
-from django.db.models import options
-from django.db.models.deletion import ProtectedError
-
-options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('offstudy_model',)
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
+from dateutil.relativedelta import relativedelta
 
 
 class OffstudyError(Exception):
@@ -29,8 +28,8 @@ class OffstudyModelMixin(SubjectIdentifierModelMixin, models.Model):
     offstudy_datetime = models.DateTimeField(
         verbose_name="Off-study Date",
         validators=[
-            date_not_before_study_start,
-            date_not_future])
+            datetime_not_before_study_start,
+            datetime_not_future])
 
     reason = models.CharField(
         verbose_name="Please code the primary reason participant taken off-study",
@@ -49,21 +48,18 @@ class OffstudyModelMixin(SubjectIdentifierModelMixin, models.Model):
             raise OffstudyError('Offstudy date may not be before the date of consent. Got {}.'.format(
                 timezone.localtime(self.offstudy_datetime).strftime(self.dateformat)))
         app_config = django_apps.get_app_config('edc_appointment')
+        self.validate_offstudy_datetime()
         Appointment = app_config.model
-        try:
-            Appointment.objects.delete_for_subject_after_date(
-                self.subject_identifier, self.offstudy_datetime, self._meta.visit_schedule_name)
-        except ProtectedError:
-            raise OffstudyError(
-                'Cannot save Offstudy. Some appointments after {} '
-                'already have visit reports.'.format(self.offstudy_datetime.strftime(self.dateformat)))
+        Appointment.objects.delete_for_subject_after_date(
+            self.subject_identifier, self.offstudy_datetime)
         super(OffstudyModelMixin, self).save(*args, **kwargs)
 
     def natural_key(self):
         return (self.subject_identifier, )
 
     def __str__(self):
-        return "{0} {1}".format(self.subject_identifier, self.offstudy_datetime.strftime(self.dateformat))
+        return "{0} {1}".format(
+            self.subject_identifier, timezone.localtime(self.offstudy_datetime).strftime(self.dateformat))
 
     @property
     def consented_before_offstudy(self):
@@ -77,10 +73,29 @@ class OffstudyModelMixin(SubjectIdentifierModelMixin, models.Model):
             consent = None
         return consent
 
+    def validate_offstudy_datetime(self):
+        if relativedelta(self.offstudy_datetime, self.last_visit_datetime).days < 0:
+            raise OffstudyError('Offstudy datetime cannot precede the last visit datetime {}. Got {}'.format(
+                timezone.localtime(self.last_visit_datetime), timezone.localtime(self.offstudy_datetime)))
+
+    @property
+    def last_visit_datetime(self):
+        visit_models = []
+        max_visit_datetimes = []
+        for visit_schedule in site_visit_schedules.registry.values():
+            if visit_schedule.visit_model not in visit_models:
+                visit_models.append(visit_schedule.visit_model)
+                last_visit = visit_schedule.visit_model.objects.last_visit()
+                if last_visit:
+                    max_visit_datetimes.append(last_visit.report_datetime)
+        if max_visit_datetimes:
+            return max(max_visit_datetimes)
+        return None
+
     class Meta:
         abstract = True
-        visit_schedule_name = None
         consent_model = None
+        visit_schedule_name = None
 
 
 class OffstudyMixin(models.Model):
@@ -94,22 +109,24 @@ class OffstudyMixin(models.Model):
 
     @property
     def offstudy_model(self):
-        return django_apps.get_model(*self._meta.offstudy_model.split('.'))
+        try:
+            return self.visit.schedule.offstudy_model
+        except AttributeError:
+            return self.schedule.offstudy_model
 
     def is_offstudy_or_raise(self):
         """Return True if the off-study report exists. """
         try:
             offstudy = self.offstudy_model.objects.get(
                 offstudy_datetime__lte=self.report_datetime,
-                subject_identifier=self.get_subject_identifier()
+                subject_identifier=self.subject_identifier
             )
             raise OffstudyError(
                 'Participant was reported off study on \'{0}\'. Data reported after this date'
-                ' cannot be captured.'.format(offstudy.offstudy_datetime.strftime('%Y-%m-%d')))
+                ' cannot be captured.'.format(timezone.localtime(offstudy.offstudy_datetime).strftime('%Y-%m-%d')))
         except self.offstudy_model.DoesNotExist:
             offstudy = None
         return offstudy
 
     class Meta:
         abstract = True
-        offstudy_model = None
--- edc_offstudy/tests.py
+++ edc_offstudy/tests.py
@@ -11,6 +11,7 @@ from edc_example.models import CrfOne, SubjectOffstudy
 from edc_example.models import SubjectVisit, Appointment, Enrollment, CrfTwo
 from edc_metadata.constants import KEYED, NOT_REQUIRED
 from edc_visit_tracking.constants import SCHEDULED, LOST_VISIT, COMPLETED_PROTOCOL_VISIT
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 
 from .model_mixins import OffstudyError
 
@@ -18,11 +19,18 @@ from .model_mixins import OffstudyError
 class TestOffstudy(TestCase):
 
     def setUp(self):
+        self.visit_schedule_name = 'subject_visit_schedule'
+        self.schedule_name = 'schedule1'
         self.subject_identifier = '111111111'
+        # self.schedule = Enrollment._meta.schedule
         self.subject_consent = SubjectConsentFactory(
             subject_identifier=self.subject_identifier,
             consent_datetime=timezone.now() - relativedelta(weeks=4))
-        Enrollment.objects.create(subject_identifier=self.subject_consent.subject_identifier)
+        enrollment = Enrollment.objects.create(
+            subject_identifier=self.subject_consent.subject_identifier,
+            schedule_name=self.schedule_name,
+            report_datetime=timezone.now() - relativedelta(weeks=4))
+        self.schedule = enrollment.schedule
 
     def test_off_study_date_after_consent(self):
         """Assert can go off study a week after consent."""
@@ -49,7 +57,7 @@ class TestOffstudy(TestCase):
 
     def test_off_study_date_before_subject_visit(self):
         """Assert cannot enter off study if visits already exist after offstudy date."""
-        appointment = Appointment.objects.filter(visit_schedule_name=SubjectOffstudy._meta.visit_schedule_name).first()
+        appointment = Appointment.objects.filter(subject_identifier=self.subject_identifier).first()
         self.subject_visit = SubjectVisitFactory(
             appointment=appointment,
             visit_schedule_name=appointment.visit_schedule_name,
@@ -70,7 +78,7 @@ class TestOffstudy(TestCase):
 
     def test_off_study_date_after_subject_visit(self):
         """Assert can enter off study if visits do not exist after offstudy date."""
-        appointment = Appointment.objects.filter(visit_schedule_name=SubjectOffstudy._meta.visit_schedule_name).first()
+        appointment = Appointment.objects.filter(subject_identifier=self.subject_identifier).first()
         self.subject_visit = SubjectVisitFactory(
             appointment=appointment,
             visit_schedule_name=appointment.visit_schedule_name,
@@ -87,3 +95,42 @@ class TestOffstudy(TestCase):
             )
         except OffstudyError:
             self.fail('OffstudyError unexpectedly raised')
+
+    def test_off_study_date_deletes_unused_appointments(self):
+        """Assert deletes any unused appointments after offstudy date."""
+        self.assertEquals(Appointment.objects.filter(subject_identifier=self.subject_identifier).count(), 4)
+        appointment = Appointment.objects.filter(subject_identifier=self.subject_identifier).first()
+        self.subject_visit = SubjectVisitFactory(
+            appointment=appointment,
+            visit_schedule_name=appointment.visit_schedule_name,
+            schedule_name=appointment.schedule_name,
+            visit_code=appointment.visit_code,
+            report_datetime=timezone.now() - relativedelta(weeks=3),
+            study_status=SCHEDULED)
+        SubjectOffstudy.objects.create(
+            subject_identifier=self.subject_consent.subject_identifier,
+            offstudy_datetime=timezone.now(),
+            reason=DEAD)
+        self.assertEquals(
+            Appointment.objects.filter(subject_identifier=self.subject_identifier).count(), 1)
+
+    def test_off_study_date_deletes_unused_appointments2(self):
+        """Assert deletes any unused appointments after offstudy date."""
+        self.assertEquals(
+            Appointment.objects.filter(subject_identifier=self.subject_identifier).count(), 4)
+        appointments = Appointment.objects.filter(subject_identifier=self.subject_identifier).order_by('appt_datetime')
+        appointment_datetimes = [appointment.appt_datetime for appointment in appointments]
+        for index, appointment in enumerate(appointments[0:2]):
+            self.subject_visit = SubjectVisitFactory(
+                appointment=appointment,
+                visit_schedule_name=appointment.visit_schedule_name,
+                schedule_name=appointment.schedule_name,
+                visit_code=appointment.visit_code,
+                report_datetime=appointment_datetimes[index],
+                study_status=SCHEDULED)
+        SubjectOffstudy.objects.create(
+            subject_identifier=self.subject_consent.subject_identifier,
+            offstudy_datetime=appointment_datetimes[2],
+            reason=DEAD)
+        self.assertEquals(
+            Appointment.objects.filter(subject_identifier=self.subject_identifier).count(), 2)
