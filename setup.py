c3cb70ae21e5ed3279df3e9dd779628426804982
erikvw <ew2789@gmail.com>
2018-08-07 16:30:44 -0500

    setup, install_requires, requirements, PEP8

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     long_description=README,
     zip_safe=False,
     keywords='django participant off-study',
+    install_requires=['edc-base'],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
