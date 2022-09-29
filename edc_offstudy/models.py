021b22eccb66a7557240214264f5aa9cca24eb5d
erikvw <ew2789@gmail.com>
2022-09-29 14:19:54 -0500

    remove SiteModelMixin from mixin, use subject identifier model manager instead of a local CurrentSiteManager

--- edc_offstudy/models.py
+++ edc_offstudy/models.py
@@ -1,10 +1,11 @@
 from edc_action_item.models import ActionNoManagersModelMixin
 from edc_consent.model_mixins import RequiresConsentFieldsModelMixin
+from edc_identifier.managers import SubjectIdentifierManager
 from edc_model import models as edc_models
 from edc_sites.models import CurrentSiteManager, SiteModelMixin
 
 from .constants import END_OF_STUDY_ACTION
-from .model_mixins import OffstudyModelManager, OffstudyModelMixin
+from .model_mixins import OffstudyModelMixin
 
 
 class SubjectOffstudy(
@@ -19,7 +20,7 @@ class SubjectOffstudy(
 
     on_site = CurrentSiteManager()
 
-    objects = OffstudyModelManager()
+    objects = SubjectIdentifierManager()
 
     history = edc_models.HistoricalRecords()
 
